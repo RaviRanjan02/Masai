5c11a42a9dc3b09ace59390b02aa0ab14f1fdd0a
RaviRanjan <raviranjan1140@gmail.com>
2021-12-05 21:48:09 +0530

    print a reverse strings

---
+++ Unit1/Coding/print_char_str_reverse.js
@@ -0,0 +1,17 @@
+// You are given a string, whose size is stored in a variable with the name N
+
+// You have to print all the characters in the string in reverse order, on a new line
+
+// For example, consider the string stored instr = "nrupul", and the value stored in N = 6, then the required output will be
+
+// l
+// u
+// p
+// u
+// r
+// n
+var N=6;
+var str="nrupul";
+for(var i=N-1;i>=0;i--){
+    console.log(str[i]);
+}
