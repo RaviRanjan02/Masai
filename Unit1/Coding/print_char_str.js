a1d3028f8d05f8338d4de083fdff2f1b046966e7
RaviRanjan <raviranjan1140@gmail.com>
2021-12-05 21:37:46 +0530

    print char in string

---
+++ Unit1/Coding/print_char_str.js
@@ -0,0 +1,19 @@
+// You are given a string, whose size is stored in a variable with the name N
+
+// The string itself is stored in a variable with the name,str
+
+// Print all the characters, stored instr, on a new line
+
+// For example, if the value stored in N = 6, and the string is str = "nrupul", then the required output will be
+
+// n
+// r
+// u
+// p
+// u
+// l
+var N=6;
+var str="ankush";
+for(var i=0;i<N;i++){
+    console.log(str[i]);
+}
