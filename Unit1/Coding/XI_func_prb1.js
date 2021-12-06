04883ed3dc851e16f2f05cad865e5fdc19a55f45
RaviRanjan <raviranjan1140@gmail.com>
2021-12-06 23:46:38 +0530

    sum of number usings functions

---
+++ Unit1/Coding/XI_func_prb1.js
@@ -0,0 +1,10 @@
+// Write a function named add Numbers which takes two numbers as input and return their sum
+// Use the function to calculate the sum of given three numbers
+
+var a=5;
+var b=7;
+
+function sum(add) {
+    return a+b;
+}
+console.log(sum(5,7)+3)
