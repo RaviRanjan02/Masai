aa2ec442ad8d9440358ac311cff9a435fe9a0779
RaviRanjan <raviranjan1140@gmail.com>
2021-12-06 10:58:09 +0530

    check in arr that 42 is present in arr or not

---
+++ Unit1/Dsa/equal_42_or_not.js
@@ -0,0 +1,17 @@
+// You are given an array of N integers. You need to print Yes if 42 is present in array, else print No.
+
+var N=[23,82,28,42,66]
+for(var i=0;i<N.length;i++){
+    if(N[i]==42){
+        console.log("Yes");
+        break;
+    }
+    else{
+        if(N[i]!=42){
+            console.log("No");
+            
+            
+        }
+    }
+}
+
