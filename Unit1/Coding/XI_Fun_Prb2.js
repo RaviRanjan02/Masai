bab95d9f62d77c9d18cbc00a4b20430c3facb2ab
RaviRanjan <raviranjan1140@gmail.com>
2021-12-06 23:47:52 +0530

    function for absolute diff

---
+++ Unit1/Coding/XI_Fun_Prb2.js
@@ -0,0 +1,12 @@
+// Write code to find the absolute difference of two numbers
+// Sample Input-1 12,4
+// Sample Output-1 8
+// Sample Input-2 4,18
+// Sample Output-2 14
+
+var a=12;
+var b=4;
+function absolutediff(a,b) {
+    return Math.abs(a-b)
+}
+console.log(absolutediff(4,18))
