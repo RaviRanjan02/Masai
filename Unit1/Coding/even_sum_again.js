8126a0a66d6b4a40bcb789bb25a7081eb7bcf293
RaviRanjan <raviranjan1140@gmail.com>
2021-12-07 21:49:16 +0530

    even sum again

--- Unit1/Coding/even_sum_again.js
+++ Unit1/Coding/even_sum_again.js
@@ -15,3 +15,17 @@
 // 0
 // 2
 // 2
+
+var num=4;
+for(var i=1;i<=num;i++){
+    var sum=0;
+    for(var j=1;j<=i;j++){
+        if(j%2==0){
+            sum=sum+j;
+            // console.log(j);
+            // break;
+        }
+        
+    }
+    console.log(sum);
+}
