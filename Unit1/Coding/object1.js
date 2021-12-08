ab73ab70e1f834fa879c68241e49220ce8b48bea
RaviRanjan <raviranjan1140@gmail.com>
2021-12-08 23:20:49 +0530

    facebook login user page

---
+++ Unit1/Coding/object1.js
@@ -0,0 +1,10 @@
+var Facebook_user={
+FirstName:"Ravi",
+lastName:"Ranjan",
+Mobile_Number: "943066437",
+Age:26,          
+NewPassword:"ghfgfggg",
+Date_of_birth:"15-10-1990",
+Gender:"male"
+}
+console.log(Facebook_user);
