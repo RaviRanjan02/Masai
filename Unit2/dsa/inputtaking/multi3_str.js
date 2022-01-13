ec56dfcbeedf769d4e094f91ee4bff88b024876c
RaviRanjan <raviranjan1140@gmail.com>
2022-01-13 23:08:45 +0530

    multiple test

---
+++ Unit2/dsa/inputtaking/multi3_str.js
@@ -0,0 +1,36 @@
+function runProgram(input) {
+    var newInput=input.trim().split("\n");
+    var test= +newInput[0];
+    console.log(test);
+    for(var i=1;i<newInput.length;i++){
+        
+        var str= newInput[i].trim();
+        console.log(str);
+    }
+    
+   
+   
+   
+   }
+   if (process.env.USERNAME === "Ravi") {
+       runProgram(` 2
+       my name is ankush
+       my name is rutwik `);
+   } else {
+       process.stdin.resume();
+       process.stdin.setEncoding("ascii");
+       let read = "";
+       process.stdin.on("data", function (input) {
+           read += input;
+       });
+       process.stdin.on("end", function () {
+           read = read.replace(/\n$/, "");
+           read = read.replace(/\n$/, "");
+           runProgram(read);
+       });
+       process.on("SIGINT", function () {
+           read = read.replace(/\n$/, "");
+           runProgram(read);
+           process.exit(0);
+       });
+   }
