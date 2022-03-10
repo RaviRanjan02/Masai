8c55b2ba30ad7328278a7426d53a9a41be6f4219
Ravi Ranjan <raviranjan1140@gmail.com>
2022-03-10 22:24:18 +0530

    minimum search in rotated array

---
+++ unit 4/DSA/minimum_search_rotated_array.js
@@ -0,0 +1,54 @@
+function minimumsearch(arr) {
+   
+    var start=0;
+    var end=arr.length-1
+    var res=arr[0]
+    while(start<=end){
+        var mid=start+(parseInt(end-start)/2)
+        if(arr[mid]<res){
+           res=arr[mid]
+           end =mid-1
+        }
+        else{
+            start=mid+1
+        }
+        
+    }
+    return res
+    
+}
+
+
+function runProgram(input) {
+   
+    var newInput=input.trim().split("\n")
+    // var  row=4
+    var arr=newInput[1].trim().split(" ").map(Number)
+    // console.log(arr);
+    console.log(minimumsearch(arr));
+}
+
+
+ 
+
+if (process.env.USERNAME === "Ravi") {
+  runProgram(`10
+  4 6 7 9 10 -1 0 1 2 3`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
